77ae0f168b1d85d869052a0555df103de27749da
MarharytaKohut <mrhrtkohut@gmail.com>
2023-06-09 22:51:56 +0300

    commit

---
+++ Src/Rugby.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Rugby {
+    public static void main(String[] args) {
+
+        int[] Red = new int[]{18, 18, 18, 25, 28, 33, 40, 37, 29, 39, 21, 22, 40, 37, 31, 29, 30, 18, 24, 27, 30, 24, 34, 37, 29};
+        int[] Blue = new int[]{31, 23, 25, 26, 39, 40, 29, 20, 36, 31, 40, 18, 20, 26, 22, 38, 34, 40, 19, 20, 18, 38, 27, 22, 31};
+        System.out.println(Arrays.toString(Red));
+        System.out.println(Arrays.toString(Blue));
+        //double average = (double) (Red + Blue) / 2;
+        //System.out.println(average);
+    }
+}
